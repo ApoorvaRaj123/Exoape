4dc40f7cc49e52e17377bd2d7ea8158ab70fd651
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-12-08 22:14:12 +0530

    Add navbar in App.jsx

--- exoape/src/App.jsx
+++ exoape/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 
 function App() {
 
   return (
     <>
-      
+      <div className="w-full h-screen bg-zinc-900">
+      <Navbar />
+      </div>
     </>
   )
 }
